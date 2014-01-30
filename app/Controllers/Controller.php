53a3fcda22c9bf177d8a7a598b9208b8ca36cc3c
Antoine Wattier <wattier.antoine@gmail.com>
2014-01-30 19:40:21 +0100

    Refactoring MVC bis

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+class Controller{
+	protected $model;
+
+	protected function __construct(){
+		$modelName=substr(get_class($this),0,strpos(get_class($this),'C')).'Model';
+		if(class_exists($modelName)){
+			$this->model=new $modelName();
+		} 
+	}
+}
