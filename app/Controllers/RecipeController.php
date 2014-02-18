21dbe5254009188483a907a7bfd69e4c85ddfd33
Antoine Wattier <wattier.antoine@gmail.com>
2014-02-19 00:52:13 +0100

    Update recipe submit and add steps

--- app/Controllers/RecipeController.php
+++ app/Controllers/RecipeController.php
@@ -16,7 +16,7 @@ class RecipeController extends Controller{
 
 				//Si l'enregistrement a réussi on redirige l'user vers sa recette
 				if($recipe){						
-					$f3->reroute('/recipe/getRecipe/'.$recipe->recipe_id);
+					$f3->reroute('/recipe/getRecipe/'.$recipe->id_recipe);
 				} 
 				break;
 			
@@ -24,27 +24,30 @@ class RecipeController extends Controller{
 				// if(isset($_GET['e']) && isset($_GET['id']) && $_GET['id'] == $f3->get('SESSION.id_user')){
 				// 	$f3->set('recipe',$this->model->getRecipe(array('id'=>$_GET['e'])));
 				// }
-				echo View::instance()->render('recipe.html');
+				if(!$f3->get('SESSION.id_user'))
+					$f3->reroute('/user/register');
+				echo View::instance()->render('Recipe/submitRecipe.html');
 		}
 		
 	}
 
 	function getRecipe($f3){
 		$f3->set('recipe',$this->model->getRecipe($f3->get('PARAMS')));
-		echo View::instance()->render('viewrecipe.html');
+		$f3->set('steps',$this->model->getRecipeSteps($f3->get('PARAMS')));
+		echo View::instance()->render('Recipe/viewRecipe.html');
 	}
 
 	function getRecipes($f3){
 		$f3->set('recipes',$this->model->getRecipes($f3->get('PARAMS')));
-		echo View::instance()->render('viewrecipes.html');
+		echo View::instance()->render('Recipe/viewRecipes.html');
 	}
 
 	function getRecipesByUser($f3){
 		$f3->set('recipes',$this->model->getRecipesByUser($f3->get('PARAMS')));
 	}
-	
+
 	function getRecipesByFilter($f3){
 		$f3->set('recipes',$this->model->getRecipesByFilter($f3->get('PARAMS')));
-		echo View::instance()->render('viewrecipes.html');
+		echo View::instance()->render('Recipe/viewRecipes.html');
 	}	
 }
--- app/Models/RecipeModel.php
+++ app/Models/RecipeModel.php
@@ -11,19 +11,34 @@ class RecipeModel extends Model{
 
 	function submit($params){
 		$this->mapper->reset();
-		$_POST['slug_recipe'] = strtolower($params['name_recipe']);
+		$_POST['slug_recipe'] =  Tools::slugify(strtolower($params['name_recipe']));
 		$_POST['id_user'] = $params['id_user'];
 		$this->mapper->copyfrom('POST',function($val) {
 		    return array_intersect_key($val, array_flip(
-		    	array('name_recipe','slug_recipe','numberOfPeople_recipe','id_user'))
+		    	array('name_recipe','slug_recipe','numberOfPeople_recipe','preparationTime_recipe','id_user'))
 		    );
 		});
 		$this->mapper->save();
+		$step_mapper = $this->getMapper('STEP');
+		foreach ($_POST['step_recipe'] as $order => $content) {
+			if(!empty($content)){	
+				$step_mapper->reset();
+				$step_mapper->order_step = $order+1;
+				$step_mapper->content_step = $content;
+				$step_mapper->id_recipe = $this->mapper->last()->id_recipe;
+				$step_mapper->save();
+			}
+		}
 		return $this->mapper;
 	}
 
 	function getRecipe($params){
 		return $this->mapper->load(array("id_recipe = :id", ':id' => $params['id']));
+	}
+
+	function getRecipeSteps($params){
+		$step_mapper = $this->getMapper('STEP');
+		return $step_mapper->find(array("id_recipe = :id", ':id' => $params['id']),array('order'=>'order_step'));
 	}
 
 	function getRecipes(){
