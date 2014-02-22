d9c06e473974d8b4140b4645944a8441a3750cd6
Antoine Wattier <wattier.antoine@gmail.com>
2014-02-22 14:43:36 +0100

    Handle HTTP Errors

--- index.php
+++ index.php
@@ -9,7 +9,14 @@ $f3->config('config/config.ini');
 // Define routes
 $f3->config('config/routes.ini');
 
+//Handle errors
+$f3->set('ONERROR',function($f3){
+    echo \View::instance()->render('error.html');
+});
+
 // Execute application
 $f3->run();
+
+
 
 ?>
